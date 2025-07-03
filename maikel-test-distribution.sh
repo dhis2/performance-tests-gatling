29aa6f5ac55c8eae79b92cbb210466e566b418a8
teleivo <teleivo@users.noreply.github.com>
2025-07-03 14:52:36 +0200

    rinse and repeat

--- maikel-test-distribution.sh
+++ maikel-test-distribution.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+# gather some more runs to look at the distribution and at the variability
 
-mvn clean
-# TODO repeat this every 20 times
-mvn gatling:test \
- -Dgatling.simulationClass=org.hisp.dhis.test.GetRawSpeedTest \
- -Dversion=42.0.0 -Dbaseline=41.0.0 -Dinstance=https://test.performance.dhis2.org/42.0_hmis \
- -Dscenario=test-scenarios/hmis/analytics-en-query-4-speed-get-test.json
+for i in {1..10}; do
+    echo "Running test iteration $i/10"
+    mvn gatling:test \
+     -Dgatling.simulationClass=org.hisp.dhis.test.GetRawSpeedTest \
+     -Dversion=42.0.0 -Dbaseline=41.0.0 -Dinstance=https://test.performance.dhis2.org/42.0_hmis \
+     -Dscenario=test-scenarios/hmis/analytics-en-query-4-speed-get-test.json
+
+    if [ $i -lt 10 ]; then
+        echo "Waiting 1 minute before next test..."
+        sleep 60
+    fi
+done
 
