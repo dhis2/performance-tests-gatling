cdf4d371fef02e2f1f8e3ae595112a7be02fcc2b
teleivo <teleivo@users.noreply.github.com>
2025-07-04 15:26:29 +0200

    extract run vars

--- maikel-test-distribution.sh
+++ maikel-test-distribution.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # gather some more runs to look at the distribution and at the variability
 
-for i in {1..10}; do
-    echo "Running test iteration $i/10"
+RUNS=24
+for i in $(seq 1 $RUNS); do
+    echo "Running test iteration $i/$RUNS"
     mvn gatling:test \
      -Dgatling.simulationClass=org.hisp.dhis.test.GetRawSpeedTest \
      -Dversion=42.0.0 -Dbaseline=41.0.0 -Dinstance=https://test.performance.dhis2.org/42.0_hmis \
      -Dscenario=test-scenarios/hmis/analytics-en-query-4-speed-get-test.json
 
-    if [ $i -lt 10 ]; then
-        echo "Waiting 1 minute before next test..."
-        sleep 60
+    if [ "$i" -lt "$RUNS" ]; then
+        echo "Waiting 5 minutes before next test..."
+        sleep 5m
     fi
 done
 
