c11d29c73dbf0b498850343f8358c3ca738ca899
teleivo <teleivo@users.noreply.github.com>
2025-07-05 05:46:24 +0200

    comment

--- experiment-raw-speed-tests-run.sh
+++ experiment-raw-speed-tests-run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # run a single GetRawSpeedTest scenario on the test.performance.dhis2.org server to compare actual
 # results against previous claims and look at variability
+#
+# To find the tomcat port for the DHIS2 instance you are looking for you can do (there might be better
+# ways)
+# ps aux | grep tomcat
+# sudo netstat -a -p -n | grep 498564
+# (it is likely an 8xxx port)
 
 RUNS=24
 
