6b8fac7b7a709784b2c3a9d5855ca7567067f31b
teleivo <teleivo@users.noreply.github.com>
2025-07-07 10:18:12 +0200

    add scripts to test TrackerExporterTests simulation

---
+++ experiment-tracker-exporter-tests-run-docker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# Run TrackerExporterTests simulation on the test.performance.dhis2.org server to investigate
+# variability when running them inside of Docker.
+# Run like:
+#
+# ./experiment-tracker-exporter-tests-run-docker.sh -Dgatling.resultsFolder=target/gatling/42.0_sl_no_docker
+
+cleanup() {
+   echo ""
+   echo "CTRL+C pressed, clean up things before exiting..."
+   exit 1 # this is only ok as I don't care for other scripts calling this script
+}
+
+trap cleanup SIGINT
+
+RUNS=24
+
+mvn clean
+
+# https://test.performance.dhis2.org/42.0_sl uses this DB
+# performance/2.39.6-analytics-be-dhis2-db-sierra-leone.sql.gz
+# not sure how it differs from the DHIS2_DB_DUMP_URL one
+DHIS2_IMAGE=dhis2/core:42.0 \
+DHIS2_DB_DUMP_URL=https://databases.dhis2.org/sierra-leone/2.42.0/dhis2-db-sierra-leone.sql.gz \
+docker compose up --detach --remove-orphans db
+
+for ((i=1; i<=RUNS; i++)); do
+  echo "Running test iteration $i/$((RUNS+1))"
+    DHIS2_IMAGE=dhis2/core:42.0 \
+    docker compose up --force-recreate --detach web
+    echo "Waiting for DHIS2 to start..."
+    timeout 300 bash -c 'until docker compose ps web | grep -q "healthy"; do sleep 10; echo "Still waiting..."; done'
+    echo "DHIS2 is ready!"
+
+    # TODO not sure if I can open that port 8080 or if its already taken by tomcats running on the
+    # server
+    mvn gatling:test \
+     -Dgatling.simulationClass=org.hisp.dhis.test.TrackerExporterTests \
+     -Dinstance=http://localhost:8080
+     "$@"
+
+    # always start a new DHIS2 container
+    docker compose rm --stop --force web
+
+    if [ "$i" -lt "$RUNS" ]; then
+      echo "Waiting 5 minutes before next run $((i+1))/$((RUNS+1))"
+        sleep 5m
+    fi
+done
+
+docker compose stop db
---
+++ experiment-tracker-exporter-tests-run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Run TrackerExporterTests simulation on the test.performance.dhis2.org server to investigate
+# variability when running them without and inside of Docker.
+# Run without name resolution like:
+#
+# ./experiment-tracker-exporter-tests-run.sh -Dinstance=http://127.0.0.1:8103/42.0_hmis -Dversion=42.0.0 -Dbaseline=41.0.0 -Dgatling.resultsFolder=target/gatling/42.0_sl_no_docker
+#
+# To find the tomcat port for the DHIS2 instance you are looking for you can do (there might be better
+# ways):
+#
+# ps aux | grep tomcat
+# sudo netstat -a -p -n | grep 498564
+#
+# (it is likely an 8xxx port)
+
+RUNS=24
+
+mvn clean
+for ((i=1; i<=RUNS; i++)); do
+  echo "Running test iteration $i/$((RUNS+1))"
+    mvn gatling:test \
+     -Dgatling.simulationClass=org.hisp.dhis.test.TrackerExporterTests \
+     "$@"
+
+    if [ "$i" -lt "$RUNS" ]; then
+      echo "Waiting 5 minutes before next run $((i+1))/$((RUNS+1))"
+        sleep 5m
+    fi
+done
+
