e5fb38e9830ec71b88445cb8757a507ee7ad3788
teleivo <teleivo@users.noreply.github.com>
2025-07-08 08:02:13 +0200

    improve comment

--- experiment-tracker-exporter-tests-run.sh
+++ experiment-tracker-exporter-tests-run.sh
@@ -3,7 +3,7 @@
 # variability when running them without Docker.
 # Run without name resolution like:
 #
-# ./experiment-tracker-exporter-tests-run.sh -Dinstance=http://127.0.0.1:8103/42.0_hmis -Dversion=42.0.0 -Dbaseline=41.0.0 -Dgatling.resultsFolder=target/gatling/42.0_sl_no_docker
+# ./experiment-tracker-exporter-tests-run.sh -Dinstance=http://127.0.0.1:8103/42.0_sl -Dgatling.resultsFolder=target/gatling/42.0_sl_no_docker
 #
 # To find the tomcat port for the DHIS2 instance you are looking for you can do (there might be better
 # ways):
